297e572e6e916194c6a7cc706d2ab2ed3c035636
Karn Yongsiriwit <karn.y@rsu.ac.th>
2021-03-22 23:37:13 +0700

    Refactor CatCard.js

--- CatCard.js
+++ CatCard.js
@@ -6,7 +6,9 @@ export default class CatCard extends Component {
     return (
       <CardItem>
         <Left>
-          <Thumbnail source={{uri: 'https://robohash.org/'+this.props.id+'?set=set4'}} style={{height: 170, width: null, flex: 1}}/>
+          <Thumbnail source={{uri: 'https://robohash.org/'+this.props.id+'?set=set4'}} 
+            style={{height: 170, width: null, flex: 1}}
+          />
           <Body>
             <Text>{this.props.name}</Text>
             <Text note>{this.props.email}</Text>
